73744af4152390e426eaa9f0db7ee6a1cee12c4b
Ayub <ayubola@gmail.com>
2024-12-19 13:09:15 -0800

    ayub

---
+++ script.js
@@ -0,0 +1,20 @@
+const increaseBtn = document.getElementById("increaseBtn")
+const decreaseBtn = document.getElementById("decreaseBtn")
+const resetBtn = document.getElementById("resetBtn")
+const noBtn = document.getElementById("noBtn")
+let count = 0
+
+increaseBtn.onclick = () => {
+    count++;
+    noBtn.textContent = count
+}
+decreaseBtn.onclick = () => {
+    count--;
+    noBtn.textContent = count
+}
+resetBtn.onclick = () => {
+    count = 0;
+    noBtn.textContent = count
+}
+
+
